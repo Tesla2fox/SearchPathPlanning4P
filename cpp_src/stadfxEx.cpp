bdc040cac6c38c6c379bea0c48249aac4f29bc4f
Tesla2fox <stef_leon_gao@outlook.com>
2018-05-06 09:55:54 +0800

    create project

---
+++ cpp_src/stadfxEx.cpp
@@ -0,0 +1,7 @@
+
+#include "stadfxEx.h"
+
+namespace deg {
+	std::ofstream conf_debug("xx_PathPlanning.txt", std::ios::trunc);
+	std::ofstream conf_draw("xx_pyPath.txt", std::ios::trunc);
+}
---
+++ cpp_src/stadfxEx.h
@@ -0,0 +1,17 @@
+
+
+#ifndef STADFXEX_H
+#define STADFXEX_H
+
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+namespace deg {
+	extern std::ofstream conf_debug;
+	extern std::ofstream conf_draw;
+}
+
+
+#endif
